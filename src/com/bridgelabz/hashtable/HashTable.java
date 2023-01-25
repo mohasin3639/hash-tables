5dba7ff0da4bc34c709b9b76d67568a107dac904
mohasin3639 <mohasinsayyad3639@gmail.com>
2023-01-25 12:21:31 +0530

    ADDED:A Program to find frequency of words in given sentence

--- src/com/bridgelabz/hashtable/HashTable.java
+++ src/com/bridgelabz/hashtable/HashTable.java
@@ -1,9 +1,28 @@
 package com.bridgelabz.hashtable;
 import java.util.Hashtable;
 public class HashTable {
+    public static Hashtable<String, Integer> getWordFrequency(String sentence) {
+        Hashtable<String, Integer> wordFrequency = new Hashtable<String, Integer>();
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            word = word.toLowerCase();
+            if (wordFrequency.containsKey(word)) {
+                int count = wordFrequency.get(word);
+                wordFrequency.put(word, count + 1);
+            } else {
+                wordFrequency.put(word, 1);
+            }
+        }
+        return wordFrequency;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome To HashTable");
+        String sentence = "To be or not to be";
+        Hashtable<String, Integer> wordFrequency = getWordFrequency(sentence);
+        for (String word : wordFrequency.keySet()) {
+            System.out.println(word + ": " + wordFrequency.get(word));
+        }
     }
 }
+
 
 
